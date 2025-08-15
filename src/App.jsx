a49fa0325586c125bade58aeafaac345e85b4fb2
ANDREY STEVEN <andreyleon53@gmail.com>
2025-08-14 21:55:10 -0500

    Funciona o no

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,11 @@ import { Card, CardContent, CardHeader, CardTitle } from "./components/ui/card";
 import { Input } from "./components/ui/input";
 import { Label } from "./components/ui/label";
 import { Badge } from "./components/ui/badge";
-import { Button } from "./components/ui/button";
-import { Eye, EyeOff } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "./components/ui/avatar";
+import DropdownMenu from "./components/ui/dropdown-menu";
 
 import {
+  Home,
   Cookie,
   ShoppingCart,
   Package,
